a5f1869dd48a198bfc448ea2ebc568f0cd97c667
hasna <hasna.elboukhari.neuro@gmail.com>
2024-08-27 07:50:21 -0700

    file created

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -10,6 +10,10 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+import Sqlalchemy
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class DBStorage():
@@ -19,15 +23,17 @@ class DBStorage():
 
     def __init__(self):
         """ Instantiation of DBStorage class """
-        MySQL_user = getenv('HBNB_MYSQL_USER')
-        MySQL_pwd = getenv('HBNB_MYSQL_PWD')
-        MySQL_host = getenv('HBNB_MYSQL_HOST')
-        MySQL_db = getenv('HBNB_MYSQL_DB')
-        MySQL_env = getenv('HBNB_ENV')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            MySQL_user, MySQL_pwd, MySQL_host, MySQL_db), pool_pre_ping=True)
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+		format(HBNB_MySQL_USER,
+		HBNB_MYSQL_PWD,
+		HBNB_MYSQL_HOST, MYSQL_DB), pool_pre_ping=True)
         Base.metadata.create_all(self.__engine)
-        if MySQL_env == "test":
+        if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
@@ -78,3 +84,30 @@ class DBStorage():
         Task: 7. Improve engines
         """
         self.__session.close()
+    def get(self, cls, id):
+        """ Returns specified object from database.
+
+        Args:
+            cls (<BaseModel-derived>): class of object to return
+            id (str): uuid of object to return
+
+        Returns:
+            `BaseModel`-derived object of UUID `id` from database.
+        """
+        if cls in classes.values() and id and type(id) is str:
+            return (self.__session.query(cls).get(id))
+
+    def count(self, cls=None):
+        """ Returns count of all objects of a given type, or grand total if no
+        type given.
+
+        Args:
+            cls (<BaseModel-derived>): class of object to return
+
+        Returns:
+            Total count of all objects in database of type `cls`, or total of
+        all objects if no type given.
+        """
+        if cls is None:
+            return (len(self.all()))
+        return (len(self.__session.query(cls).all()))
